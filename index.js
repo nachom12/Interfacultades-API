c8d27db8d4c2a06ec58d582340471013b6b4b8f1
Juan Medina <medina189153@gmail.com>
2021-03-27 20:01:06 -0300

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const { config } = require('./config/index');
 const { teams } = require('./routes/teams.js');
 
+app.use(cors);
 //body parser
 app.use(express.json());
 
